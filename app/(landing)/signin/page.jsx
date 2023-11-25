195d1d74fdaaba2accc5732b8068035109dbce72
fckevr <151963075+fckevr@users.noreply.github.com>
2023-11-25 12:55:04 +0700

    Add files via upload

--- app/(landing)/signin/page.jsx
+++ app/(landing)/signin/page.jsx
@@ -190,19 +190,3 @@ const LoginLayout = () => {
 }
 
 export default LoginLayout
-export async function getServerSideProps(context) {
-  const session = await getSession(context)
-
-  if (session) {
-    return {
-      redirect: {
-        destination: '/',
-        permanent: false,
-      },
-    }
-  }
-
-  return {
-    props: { session }
-  }
-}
