bdc5707132e1f339fefa76a372212bc5d1ac8a11
fckevr <151963075+fckevr@users.noreply.github.com>
2023-11-25 13:23:23 +0700

    Create layout.jsx

---
+++ app/phongAdmin/report/[...id]/layout.jsx
@@ -0,0 +1,15 @@
+import '@styles/globals.css';
+export const metadata = {
+    title: 'Update Report',
+  }
+
+
+  const UpdateReport = ({children}) => {
+    return (
+      <section>
+        {children}
+      </section>
+    )
+  }
+  
+  export default UpdateReport
