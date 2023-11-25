c1980aaa6236618cf6370642b623210546f14f8d
fckevr <151963075+fckevr@users.noreply.github.com>
2023-11-25 12:51:32 +0700

    Add files via upload

---
+++ app/(call)/call/[room_id]/layout.jsx
@@ -0,0 +1,15 @@
+import '@styles/globals.css';
+export const metadata = {
+    title: 'Call',
+  }
+
+
+  const CallLayout = ({children}) => {
+    return (
+      <section>
+        {children}
+      </section>
+    )
+  }
+  
+  export default CallLayout
