78d01616d3b964fe55d70cda2c4177550cae4a24
fckevr <151963075+fckevr@users.noreply.github.com>
2023-11-26 20:33:03 +0700

    Add files via upload

--- app/(dashboard)/actor/layout.jsx
+++ app/(dashboard)/actor/layout.jsx
@@ -1,23 +1,15 @@
 import '@styles/globals.css';
-export async function generateMetadata() {
-    const response = await fetch(process.env.APP_URL + "/api/actor/all", {
-      cache: "no-cache",
-    })
-    const actors = await response.json()
-    const listName = actors.map(actor => actor.name)
-    return {
-        title: 'All Actors',
-        description: listName.join(', '),
-    }
+export const metadata = {
+  title: 'All Actors',
+  description: 'All Hotest Porn stars in the world',
 }
 
+const AllActorLayout = ({children}) => {
+  return (
+    <section className='w-full flex-center'>
+      {children}
+    </section>
+  )
+}
 
-  const AllActorLayout = ({children}) => {
-    return (
-      <section className='w-full flex-center'>
-        {children}
-      </section>
-    )
-  }
-  
-  export default AllActorLayout
+export default AllActorLayout
