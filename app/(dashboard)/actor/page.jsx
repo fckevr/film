a9911c2283601a06d895ddf87daeda3a3ce63138
fckevr <151963075+fckevr@users.noreply.github.com>
2023-11-26 15:33:57 +0700

    Add files via upload

--- app/(dashboard)/actor/page.jsx
+++ app/(dashboard)/actor/page.jsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 import Link from "next/link"
 
 async function getData() {
-    let response = await fetch(process.env.APP_URL + "/api/actor/all", {next: {revalidate: 3600}})
+    let response = await fetch(process.env.APP_URL + "/api/actor/all", {next: {revalidate: 3600}, cache: "no-cache"})
     if (response.ok) {
         const allActor = await response.json()
         const actorListByName = []
